791aae182b30600a5f088bfd39d1f7e06d7c093e
maen <mk.maen93@gmail.com>
2023-06-25 18:20:38 +0200

    update dashboard add new logo

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -232,7 +232,7 @@ use({
     vim.g.floaterm_width = 0.8
     vim.g.floaterm_height = 0.8
     vim.keymap.set('n', '<Leader>t', ':FloatermToggle<CR>')
-    vim.keymap.set('t', '<Leader>t', '<C-\\><C-n>:FloatermToggle<CR>')
+    vim.keymap.set('t', '<>', '<C-\\><C-n>:FloatermToggle<CR>')
     vim.cmd([[
       highlight link Floaterm CursorLine
       highlight link FloatermBorder CursorLineBg
